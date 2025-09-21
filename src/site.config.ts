57e37edc9c0b7953c66bf511f66ba4f1523ac15b
ArronHC <arronhc@qq.com>
2025-09-21 22:32:35 +0800

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -116,7 +116,7 @@ export const integ: IntegrationUserConfig = {
     applyTip: [
       { name: 'Name', val: theme.title },
       { name: 'Desc', val: theme.description || 'Null' },
-      { name: 'Link', val: 'https://astro-pure.js.org/' },
+      { name: 'Link', val: 'https://arronhc.cyou/' },
       { name: 'Avatar', val: 'https://astro-pure.js.org/favicon/favicon.ico' }
     ]
   },
